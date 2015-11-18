82112f0428fc524db48bd6421e972202aa5eaeaf
mwinchester96 <mwinchester@wesleyan.edu>
2015-11-17 23:52:27 -0500

    first commit of realtor

--- app/models/scotch.rb
+++ app/models/scotch.rb
@@ -1,9 +1,4 @@
 class Scotch < ActiveRecord::Base
   has_many :reviews
 
-  has_attached_file :image, :styles => { :medium => "300x300", :thumb => "100x100>" },
-    :default_url => "/images/:style/missing.jpg", :storage => :s3,
-    :s3_credentials => 'config/s3_config.yml'
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-
 end
