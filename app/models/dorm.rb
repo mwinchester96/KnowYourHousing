70857288b36e695386629047b7abac2d3c4a57eb
mwinchester96 <mwinchester@wesleyan.edu>
2015-11-22 21:54:44 -0500

    changed dorm search method

--- app/models/dorm.rb
+++ app/models/dorm.rb
@@ -2,7 +2,7 @@ class Dorm < ActiveRecord::Base
   has_many :dorm_reviews
 
   def self.search(search)
-    where("lower(college) LIKE ?", "%#{search.downcase}%")
+    where("lower(college) LIKE ? OR lower(name) LIKE ?", "%#{search.downcase}%", "%#{search.downcase}%")
   end
 
 end
