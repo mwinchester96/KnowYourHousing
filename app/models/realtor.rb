de6acac4a3749d6a7f02eeafab18a326afb727fd
mwinchester96 <mwinchester@wesleyan.edu>
2015-11-22 21:40:35 -0500

    changed realtor search

--- app/models/realtor.rb
+++ app/models/realtor.rb
@@ -2,7 +2,7 @@ class Realtor < ActiveRecord::Base
   has_many :reviews
   
   def self.search(search)
-    where("lower(college) LIKE ?", "%#{search.downcase}%")
+    where("lower(college) LIKE ? OR lower(name) LIKE ?", "%#{search.downcase}%","%#{search.downcase}%" )
   end
 
 end
